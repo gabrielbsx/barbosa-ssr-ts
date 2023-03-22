0567f3d4e55b7aa72b84a4c85eb5fb4220579f2b
Gabriel Barbosa <gabrielturing@gmail.com>
2023-03-22 04:02:28 -0300

    refactor: remove log home/example controller

--- server/src/modules/home.controller.ts
+++ server/src/modules/home.controller.ts
@@ -3,7 +3,6 @@ import { ok } from '../helpers'
 
 class HomeController implements Controller {
   async handle (httpRequest: Request): Promise<Response> {
-    console.log(httpRequest)
     return ok({})
   }
 }
