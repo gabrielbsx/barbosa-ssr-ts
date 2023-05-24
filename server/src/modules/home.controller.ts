b0552548f03b99b8547a61ba9acb6d05a48ba5c3
Gabriel <gabrielturing@gmail.com>
2023-05-24 00:30:02 -0300

    feat: add endpoint and method

--- server/src/modules/home.controller.ts
+++ server/src/modules/home.controller.ts
@@ -1,8 +1,11 @@
-import { type Controller, type Request, type Response } from '../contracts'
+import { type Method, type Controller, type Request, type Response } from '../contracts'
 import { ok } from '../helpers'
 
 class HomeController implements Controller {
+  static endpoint = '/'
+  static method: Method = 'post'
   async handle (httpRequest: Request): Promise<Response> {
+    console.log(httpRequest)
     return ok({})
   }
 }
